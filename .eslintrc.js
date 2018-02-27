8f633acfc9c6b694d18ab1048e463e0400c4c562
Alexander Tegelid <alex@tegelid.se>
2018-02-27 08:54:27 +0100

    Add eslint rules
    Specify the cumaLocalizedWarnings variable as global since it's defined
    in WordPress via wp_localize_script()

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+	"globals": {
+		"$": false,
+		"jQuery": false,
+		"cumaLocalizedWarnings": true
+	},
+	"extends": "eslint:recommended",
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"eqeqeq": [
+			"error"
+		],
+		"curly": [
+			"error"
+		],
+		"camelcase": [
+			"error"
+		],
+		"brace-style": [
+			"error",
+			"1tbs"
+		],
+		"object-curly-spacing": [
+			"error",
+			"never"
+		],
+		"yoda": [
+			"error",
+			"never"
+		]
+	}
+};
